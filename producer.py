eb041e0da48e7fdf4b8ae909a2aad62a87b2d995
Madhu Pocham <madhupocham@gmail.com>
2022-03-30 09:57:34 -0400

    learn

--- producer.py
+++ producer.py
@@ -8,12 +8,14 @@ def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
 
-producer = KafkaProducer(bootstrap_servers=['192.168.0.10:9092'],
+producer = KafkaProducer(bootstrap_servers='kafka-1:9092,kafka-2:9092',
                          value_serializer=json_serializer)
 
 if __name__ == "__main__":
     while 1 == 1:
         registered_user = get_registered_user()
         print(registered_user)
-        producer.send("registered_user", registered_user)
+        future = producer.send("registered_user", registered_user)
+        result = future.get(timeout=60)
+        print(result)
         time.sleep(4)
